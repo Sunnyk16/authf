9e38f4efd8b14125aa881a6d1d8326054d10bc6a
Sunnyk16 <kumarsunny1246@gmail.com>
2024-09-29 00:57:00 +0530

    Update page layout and styling in auth component

--- src/components/main/auth.jsx
+++ src/components/main/auth.jsx
@@ -3,13 +3,13 @@
 
 const Auth = () => {
     return (
-        <div className="flex gap-2 px-2 py-2 ">
+        <div className="flex flex-col md:flex-row gap-4 px-2 mx-2 ">
             {/* container 1 */}
-            <div className="w-1/2  bg-gray-100 rounded-sm">
+            <div className="w-full md:w-1/2 md:p-4 h-screen bg-gray-100 rounded-sm">
                 {/* header */}
                 <main>
-                    <div className="columns-2 gap-2 px-1 py-1">
-                        <div className="w-full h-5">
+                    <div className="flex flex-col md:flex-row justify-between items-center gap-2 px-1 py-1 ">
+                        <div className="w-full mb-2 h--3 md:mb-0 md:h-2">
                             <img></img>
                             <p className="font-bold p-2 text-2xl capitalize">
                                 <span className="text-green-600">regions</span> bank
@@ -17,16 +17,16 @@ const Auth = () => {
 
                         </div>
 
-                        <div className="  w-full h-5 flex justify-evenly  ">
+                        <div className="  w-full h-5 flex justify-evenly hidden lg:flex  ">
                             <p className="font-semibold text-l text-center text-gray-500 p-2">are you a new customer?</p>
-                            <button className="bg-white text-green-600 rounded-full w-auto h-10 p-1 px-2 shadow-sm font-bold  text-center capitalize ">enroll now</button>
+                            <button className="bg-white text-green-600 rounded-full w-36 h-10 p-1 px-2 shadow-sm shadow-gray-400 font-bold  text-center capitalize ">enroll now</button>
                         </div>
                     </div>
 
                 </main>
 
                 {/* body */}
-                <div className="w-full columns-1 ">
+                <div className="w-full columns-1 mt-10 ">
 
                     <div className="text-center">
                         <p className="text-3xl font-extrabold">welcome to bank</p>
@@ -36,25 +36,14 @@ const Auth = () => {
                     </div>
 
 
-                    {/* <div className="w-full columns-1 flex flex-col mt-4 justify-center items-center">
-                        <div className="relative w-full mb-4 flex justify-center">
-                            <input type="text" id="username" className="peer w-96 h-12 p-3  border-gray-300 rounded-md text-base sm:text-lg md:text-xl focus:outline-none focus:border-green-500" placeholder=" " />
-                            <label htmlFor="username" className="absolute left-3 top-3 text-gray-500 peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-focus:top-[-10px] peer-focus:text-sm peer-focus:text-green-600 transition-all">Username</label>
-                        </div>
-                        <div className="relative w-full mb-6 flex justify-center">
-                            <input type="password" id="password" className="peer w-96 h-12 p-3  border-gray-300 rounded-md text-base sm:text-lg md:text-xl focus:outline-none focus:border-green-500" placeholder=" " />
-                            <label htmlFor="password" className="absolute left-3 top-3 text-gray-500 peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-focus:top-[-10px] peer-focus:text-sm peer-focus:text-green-600 transition-all">Password</label>
-                        </div>
-                        <button className="w-96 h-12 bg-green-600 text-white rounded-full  mt-4 hover:bg-green-700 transition-all">Sign In</button>
-
-                    </div> */}
+                    
                     <div className="w-full columns-1 flex flex-col mt-4 justify-center items-center">
-                        <div className="relative w-96 mb-4">
-                            <input type="text" id="username" className="peer w-full h-12 p-3 border-gray-300 rounded-md text-base sm:text-lg md:text-xl focus:outline-none focus:border-green-500" placeholder=" " />
+                        <div className="relative w-96 mb-4 ">
+                            <input type="text" id="username" className="peer w-full h-12 p-3 border-gray-300 rounded-md text-base sm:text-lg md:text-xl focus:outline-none focus:border-green-500 shadow-sm shadow-gray-400" placeholder=" " />
                             <label htmlFor="username" className="absolute left-3 top-3 text-gray-500 peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-focus:top-[-10px] peer-focus:text-sm peer-focus:text-green-600 transition-all">Username</label>
                         </div>
                         <div className="relative w-96 mb-6">
-                            <input type="password" id="password" className="peer w-full h-12 p-3 border-gray-300 rounded-md text-base sm:text-lg md:text-xl focus:outline-none focus:border-green-500" placeholder=" " />
+                            <input type="password" id="password" className="peer w-full h-12 p-3 border-gray-300 rounded-md text-base sm:text-lg md:text-xl focus:outline-none focus:border-green-500 shadow-sm shadow-gray-400" placeholder=" " />
                             <label htmlFor="password" className="absolute left-3 top-3 text-gray-500 peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-focus:top-[-10px] peer-focus:text-sm peer-focus:text-green-600 transition-all">Password</label>
                         </div>
 
@@ -74,6 +63,20 @@ const Auth = () => {
 
                 </div>
 
+                <div className="  w-full h-5 flex justify-evenly lg:hidden  mt-4 ">
+                            <p className="font-semibold text-sm l:text-l  text-center text-gray-500 p-2 ">are you a new customer?</p>
+                            <button className="bg-white text-green-600 rounded-full w-36  h-10 p-1 px-2 shadow-sm shadow-gray-400 font-bold sm:w-28 sm:h-8 text-center capitalize ">enroll now</button>
+                        </div>
+
+                {/* footer */}
+                <div className="mt-20 md:mt-52">
+                <div className="w-full columns-1 flex justify-center items-center mt-4">
+                    <p className="text-gray-500">© 2024 Regions Bank. All Rights Reserved.</p>
+                    </div>
+
+                </div>
+                
+
 
 
 
@@ -82,7 +85,7 @@ const Auth = () => {
             </div>
 
             {/* container 2 */}
-            <div className="w-1/2 h-48 bg-red-600 rounded-sm">
+            <div className="w-full md:w-1/2 h-48 bg-red-600 rounded-sm">
 
             </div>
         </div>
